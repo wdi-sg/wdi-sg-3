17c15ce9ca2428d204139617677ff28f960b5201
Andrian Kanta <a.kanta22@gmail.com>
2016-06-06 10:58:43 +0800

    remove changes to starter code

--- day-8-callbacks-lab/starter-code/timers.js
+++ day-8-callbacks-lab/starter-code/timers.js
@@ -3,56 +3,20 @@ $( "#reset" ).click(Reset);
 $( "#start" ).click(Start);
 $( "#pause" ).click(Pause);
 
-
-var timer = {
-  Id:0,
-  time:0,
-}
-
 // Call this function to update the text display
-function UpdateText(seconds) {
+function UpdateText( seconds ) {
     $( "#timeDisplay" ).text("Time elapsed: " + seconds);
-
 }
-
-function UpdateTime(){
-  timer.time++;
-  console.log(timer.time);
-  UpdateText(timer.time);
-}
-
 
 // YOUR CODE GOES HERE
 function Reset() {
     console.log("Reset Clicked");
-    timer.time = 0;
-    window.clearInterval(timer.Id);
-    Start();
-
-    // $( "#timeDisplay" ).text("Time elapsed: " + seconds);
-
 }
 
 function Start() {
     console.log("Start Clicked");
-    //celar timer interval before
-    // window.clearInterval(timer.Id);
-    timer.Id = window.setInterval(UpdateTime,1000);
-    console.log(timer.Id);
-    // if(timerId !==0 ){
-    //   seconds = 0;
-    // }
 }
 
 function Pause() {
     console.log("Pause Clicked");
-    window.clearInterval(timer.Id);
 }
-
-
-// function UpdateTime( seconds ){
-//     $( "#timer").text(seconds)
-//     console.log(seconds);
-//     seconds++
-//     console.log(seconds);
-// }

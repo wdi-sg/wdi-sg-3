564e318b816c1fb614c1c068871cbe06885c47c0
Geng <snggeng@gmailcom>
2016-06-23 14:02:43 +0800

    saved timer.js

--- day-8-callbacks-lab/starter-code/timers.js
+++ day-8-callbacks-lab/starter-code/timers.js
@@ -1,22 +1,35 @@
 //some jquery for you - you can ignore for now. This will trigger your button clicks
-$( "#reset" ).click(Reset);
-$( "#start" ).click(Start);
-$( "#pause" ).click(Pause);
+$("#reset").click(Reset);
+$("#start").click(Start);
+$("#pause").click(Pause);
 
 // Call this function to update the text display
-function UpdateText( seconds ) {
-    $( "#timeDisplay" ).text("Time elapsed: " + seconds);
+function UpdateText () {
+  $("#timeDisplay").text('Time elapsed: ' + seconds);
 }
 
 // YOUR CODE GOES HERE
-function Reset() {
-    console.log("Reset Clicked");
+var seconds = 0;
+var timerId;
+
+function Reset () {
+  window.clearInterval(timerId);
+  console.log('Reset Clicked');
+  seconds = -1;
+  updateTime();
 }
 
-function Start() {
-    console.log("Start Clicked");
+function Start () {
+  timerId = window.setInterval(updateTime, 1000);
+  console.log('Start Clicked');
 }
 
-function Pause() {
-    console.log("Pause Clicked");
+function Pause () {
+  window.clearInterval(timerId);
+  console.log('Pause Clicked');
+}
+
+function updateTime () {
+  seconds++;
+  UpdateText();
 }

d8334d88e72a436b7d099b07526dbb04afd1a147
Gabrielle Ong <gabrielleong97@gmail.com>
2016-07-14 17:37:00 +0800

    adds airport answers

---
+++ day-37-mongoModeling-lab/solution-code 5.56.19 AM/app/models/flight.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose')
+
+var FlightSchema = new mongoose.Schema({
+  from: String,
+  to: String,
+  airline: String,
+  passengers: []
+})
+
+var Flight = mongoose.model('Flight', FlightSchema)
+
+module.exports = Flight
